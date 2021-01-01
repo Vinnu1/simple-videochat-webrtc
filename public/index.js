23880051f6d1b59be22445c2bd48b8be96b282b8
Herbrand Hofker <herbrandhofker@users.noreply.github.com>
2021-01-02 00:44:41 +0100

    stripped css

--- public/index.js
+++ public/index.js
@@ -1,14 +1,14 @@
 let Peer = require('simple-peer')
 let io = require('socket.io-client');
 let socket = io()
-const video = document.getElementById('video')
+const administratorsVideo = document.getElementById('administratorsVideo')
 let client = {}
 //get stream
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
         socket.emit('NewClient')
-        video.srcObject = stream
-        video.play()
+        administratorsVideo.srcObject = stream
+        administratorsVideo.play()
 
         //used to initialize a peer
         function InitPeer(type) {
