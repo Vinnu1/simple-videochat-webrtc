f5103159ff0057853c6a43e2c088641f168451ce
Herbrand Hofker <herbrandhofker@users.noreply.github.com>
2021-01-02 00:28:00 +0100

    more modern js

--- public/bundle.js
+++ public/bundle.js
@@ -13756,9 +13756,8 @@ yeast.decode = decode;
 module.exports = yeast;
 
 },{}],75:[function(require,module,exports){
-
 let Peer = require('simple-peer')
-let io=require('socket.io-client');
+let io = require('socket.io-client');
 let socket = io()
 const video = document.getElementById('video')
 let client = {}
@@ -13767,7 +13766,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
         socket.emit('NewClient')
         video.srcObject = stream
-        video.play()      
+        video.play()
 
         //used to initialize a peer
         function InitPeer(type) {
@@ -13785,11 +13784,44 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
                 peervideo.style.filter = decodedData
             })
             return peer
-        }
+        } 
 
-        //for peer of type init
-        function CreatePeer() {
-            client.gotAnswer = false
+        function CreateVideo(stream) {
+            let video = document.createElement('video')
+            video.id = 'peerVideo'
+            video.srcObject = stream
+            video.setAttribute('class', 'embed-responsive-item')
+            document.getElementById('peerDiv').appendChild(video)
+            video.play()
+
+            video.addEventListener('click', () => {
+                if (video.volume != 0)
+                    video.volume = 0
+                else
+                    video.volume = 1
+            })
+        }     
+
+        socket.on('Offer', (offer)=>{  //for peer of type not init
+            let peer = InitPeer('notInit')
+            peer.on('signal', (data) => {
+                socket.emit('Answer', data)
+            })
+            peer.signal(offer)
+            client.peer = peer
+        })
+
+        socket.on('Answer', (answer) => {
+            client.gotAnswer = true
+            let peer = client.peer
+            peer.signal(answer)
+        })
+
+        socket.on('SessionActive', () => {
+            document.write('Session Active. Please come back later')
+        })
+        socket.on('CreatePeer', ()=>{ //for peer of type init
+             client.gotAnswer = false
             let peer = InitPeer('init')
             peer.on('signal', function (data) {
                 if (!client.gotAnswer) {
@@ -13797,54 +13829,13 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
                 }
             })
             client.peer = peer
-        }
+         })
 
-        //for peer of type not init
-        function Offer(offer) {
-            let peer = InitPeer('notInit')
-            peer.on('signal', (data) => {
-                socket.emit('Answer', data)
-            })
-            peer.signal(offer)
-            client.peer = peer
-        }
-
-        function Answer(answer) {
-            client.gotAnswer = true
-            let peer = client.peer
-            peer.signal(answer)
-        }
-
-        function CreateVideo(stream) {         
-            let video = document.createElement('video')
-            video.id = 'peerVideo'
-            video.srcObject = stream
-            video.setAttribute('class', 'embed-responsive-item')
-            document.getElementById('peerDiv').appendChild(video)
-            video.play()
-         
-            video.addEventListener('click', () => {
-                if (video.volume != 0)
-                    video.volume = 0
-                else
-                    video.volume = 1
-            })
-        }
-
-        function DeletePeer() {
-          //  document.getElementById("peerVideo").remove();
-           if (client.peer) {
+        socket.on('DeletePeer', ()=>{
+             if (client.peer) {
                 client.peer.destroy()
             }
-        }
-
-        socket.on('Offer', Offer)
-        socket.on('Answer', Answer)
-        socket.on('SessionActive', ()=>{
-            document.write('Session Active. Please come back later')
         })
-        socket.on('CreatePeer', CreatePeer)
-        socket.on('DeletePeer', DeletePeer)
     })
     .catch(err => document.write(err))
 
--- public/index.js
+++ public/index.js
@@ -1,6 +1,5 @@
-
 let Peer = require('simple-peer')
-let io=require('socket.io-client');
+let io = require('socket.io-client');
 let socket = io()
 const video = document.getElementById('video')
 let client = {}
@@ -9,7 +8,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
         socket.emit('NewClient')
         video.srcObject = stream
-        video.play()      
+        video.play()
 
         //used to initialize a peer
         function InitPeer(type) {
@@ -27,11 +26,44 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
                 peervideo.style.filter = decodedData
             })
             return peer
-        }
+        } 
 
-        //for peer of type init
-        function CreatePeer() {
-            client.gotAnswer = false
+        function CreateVideo(stream) {
+            let video = document.createElement('video')
+            video.id = 'peerVideo'
+            video.srcObject = stream
+            video.setAttribute('class', 'embed-responsive-item')
+            document.getElementById('peerDiv').appendChild(video)
+            video.play()
+
+            video.addEventListener('click', () => {
+                if (video.volume != 0)
+                    video.volume = 0
+                else
+                    video.volume = 1
+            })
+        }     
+
+        socket.on('Offer', (offer)=>{  //for peer of type not init
+            let peer = InitPeer('notInit')
+            peer.on('signal', (data) => {
+                socket.emit('Answer', data)
+            })
+            peer.signal(offer)
+            client.peer = peer
+        })
+
+        socket.on('Answer', (answer) => {
+            client.gotAnswer = true
+            let peer = client.peer
+            peer.signal(answer)
+        })
+
+        socket.on('SessionActive', () => {
+            document.write('Session Active. Please come back later')
+        })
+        socket.on('CreatePeer', ()=>{ //for peer of type init
+             client.gotAnswer = false
             let peer = InitPeer('init')
             peer.on('signal', function (data) {
                 if (!client.gotAnswer) {
@@ -39,53 +71,12 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
                 }
             })
             client.peer = peer
-        }
+         })
 
-        //for peer of type not init
-        function Offer(offer) {
-            let peer = InitPeer('notInit')
-            peer.on('signal', (data) => {
-                socket.emit('Answer', data)
-            })
-            peer.signal(offer)
-            client.peer = peer
-        }
-
-        function Answer(answer) {
-            client.gotAnswer = true
-            let peer = client.peer
-            peer.signal(answer)
-        }
-
-        function CreateVideo(stream) {         
-            let video = document.createElement('video')
-            video.id = 'peerVideo'
-            video.srcObject = stream
-            video.setAttribute('class', 'embed-responsive-item')
-            document.getElementById('peerDiv').appendChild(video)
-            video.play()
-         
-            video.addEventListener('click', () => {
-                if (video.volume != 0)
-                    video.volume = 0
-                else
-                    video.volume = 1
-            })
-        }
-
-        function DeletePeer() {
-          //  document.getElementById("peerVideo").remove();
-           if (client.peer) {
+        socket.on('DeletePeer', ()=>{
+             if (client.peer) {
                 client.peer.destroy()
             }
-        }
-
-        socket.on('Offer', Offer)
-        socket.on('Answer', Answer)
-        socket.on('SessionActive', ()=>{
-            document.write('Session Active. Please come back later')
         })
-        socket.on('CreatePeer', CreatePeer)
-        socket.on('DeletePeer', DeletePeer)
     })
     .catch(err => document.write(err))
